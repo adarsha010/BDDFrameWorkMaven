b27bdd3a0fe69faa0d86fefed2d59859e20d6528
adarsha.naraya <Adarsha.Naraya@ticketmaster.com>
2020-05-01 08:34:41 +0530

    enhanced the browser instantiation method and updated read me

--- src/main/java/com/www/browser/Browser.java
+++ src/main/java/com/www/browser/Browser.java
@@ -18,35 +18,35 @@ public class Browser {
 	public static WebDriver driver;
 	final static Logger logger = Logger.getLogger(Browser.class);
 	private Browser() {
-		
+
 		String browser = PropertyUtil.getProperty("browser");
 		String url = PropertyUtil.getProperty("URL");
-		logger.info("Read value of browser:"+browser);
-		logger.info("Read value of application URL:" + url );
+		String platFormName = PropertyUtil.getProperty("platformName");
+		logger.info("Read value of browser:" + browser);
+		logger.info("Read value of application URL:" + url);
 
-		
-		if(browser.equals("chrome")) {
-			System.getProperty("browser");
-			System.setProperty("webdriver.chrome.driver", ".//src/test/resources/chromedriver");
-			ChromeOptions options=new ChromeOptions();
+		if (browser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/" + platFormName + "/chromedriver");
+			logger.info("Picked the chrome driver from the location  :" + System.getProperty("webdriver.chrome.driver").toString());
+			ChromeOptions options = new ChromeOptions();
 			options.addArguments("--incognito");
-			DesiredCapabilities capabilities= new DesiredCapabilities();
+			DesiredCapabilities capabilities = new DesiredCapabilities();
 			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-			driver=new ChromeDriver();
-		}else if(browser.equals("firefox")){
-			
-		}else {
-			System.out.println("Invlaid Browser");
-			System.setProperty("webdriver.gecko.driver", ".//src/test/resources/geckodriver.exe");
-			FirefoxOptions FFoptions= new FirefoxOptions();
+			driver = new ChromeDriver();
+		} else if (browser.equals("firefox")){
+			System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/" + platFormName + "/geckodriver.exe");
+			logger.info("Picked the Firefox Gecko driver from the location  :" + System.getProperty("webdriver.gecko.driver").toString());
+			FirefoxOptions FFoptions = new FirefoxOptions();
 			FFoptions.addArguments("--incognito");
-			driver= new FirefoxDriver(FFoptions);
+			driver = new FirefoxDriver(FFoptions);
+		} else {
+			logger.error("Invalid args are passed either browser|platFormName " + "Browser :" + browser + " platFormName :" + platFormName);
+			System.exit(1);
 		}
 		driver.get(url);
-		driver.manage().timeouts().pageLoadTimeout(2000,TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(2000,TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(2000, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(2000, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
 	}
 	public static void initialize() {
 		new Browser();
